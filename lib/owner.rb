b8c0df35749f260b4563a98e054ed81e1b366039
Alexis Priore <alexispriore@gmail.com>
2020-01-20 18:00:54 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,10 +1,10 @@
 class Owner
-  class Owner
+  
   attr_reader :species
-  attr_accessor :name, :pets, :fish, :dog, :cat
+  attr_accessor :name, :pets, :dog, :cat
 
   @@all = []
-  @@pets = {:fishes => [], :dogs => [], :cats => []}
+  @@pets = {:dogs => [], :cats => []}
 
   def initialize(species)
     @species = species
@@ -32,10 +32,6 @@ class Owner
     @@pets
   end
 
-  def buy_fish(fish)
-    @@pets[:fishes] << Fish.new(fish)
-  end
-
   def buy_cat(cat)
     @@pets[:cats] << Cat.new(cat)
   end
@@ -53,12 +49,6 @@ class Owner
   def play_with_cats
     @@pets[:cats].each do |cat|
       cat.mood = 'happy'
-    end
-  end
-
-  def feed_fish
-    @@pets[:fishes].each do |fish|
-      fish.mood = 'happy'
     end
   end
 
