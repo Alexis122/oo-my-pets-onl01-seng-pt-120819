d8b6686a3d0ae2db43ddfba853ee41fdbdd21649
Alexis Priore <alexispriore@gmail.com>
2020-01-20 17:58:30 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,9 @@
 class Cat
   
+  attr_reader :name, :size, :temperament
+def initialize(name, size, temperament)
+		    @name = name
+		    @size = size
+		    @temperament = temperament
+		  end
 end
